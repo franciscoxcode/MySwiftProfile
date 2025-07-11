d6c81dcf818104280019b9010985b5e7e02bf290
Francisco Casillas <chiccasillas@gmail.com>
2025-07-11 12:17:14 -0600

    Initial Commit

---
+++ MySwiftProfile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySwiftProfile
+//
+//  Created by Francisco Jean on 11/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySwiftProfile/MySwiftProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftProfileApp.swift
+//  MySwiftProfile
+//
+//  Created by Francisco Jean on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
